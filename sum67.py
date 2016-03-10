6190d9974deb14bfde6a9eda50428705a1d91a18
Ben S <ben.s.s@hotmail.co.uk>
2016-03-10 20:59:35 +0000

    Created file

---
+++ sum67.py
@@ -0,0 +1,11 @@
+def sum67(nums):
+  count = 0
+  for a in nums:
+   if a == 6:
+    while True:
+     nums.remove(nums[count])
+     if nums[count] == 7:
+      nums.remove(nums[count])
+      break
+   count += 1
+  return sum(nums)
